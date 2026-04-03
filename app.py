d7e7d17a34cfee9679652b9803e7d495b309ce0f
zcharif21 <zinebcharif07@gmail.com>
2026-04-04 00:49:46 +0100

    fix it

--- app.py
+++ app.py
@@ -275,5 +275,6 @@ def create_app() -> Flask:
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app = create_app()
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
+    app.run(host="0.0.0.0", port=port)
